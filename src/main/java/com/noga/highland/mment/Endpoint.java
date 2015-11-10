48297c4229635a4f69d31c3b008be52424b9c5d9
noga.highland@googlemail.com <noga.highland@googlemail.com>
2015-11-11 02:08:23 +0900

    message receive by websocket

--- src/main/java/com/noga/highland/mment/Endpoint.java
+++ src/main/java/com/noga/highland/mment/Endpoint.java
@@ -1,8 +1,13 @@
 package com.noga.highland.mment;
 
+import javax.websocket.OnMessage;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint(value="/")
 public class Endpoint {
-
+	
+	@OnMessage
+	public void onMessage(String message) {
+		System.out.println(message);
+	}
 }
